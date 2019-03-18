472b4b7db0f3fd7cc2cf5b54bd72df0d9392d658
cloudstar91 <natalie.hoang91@gmail.com>
2019-03-18 10:43:14 +0700

    Update mini-diary.js

--- mini-diary.js
+++ mini-diary.js
@@ -286,18 +286,14 @@ function logIn(e) {
   let displayName = document.getElementById("userName");
   if (userNameInput !== "") {
     displayName.innerHTML = userNameInput;
-    quote.innerHTML =
-<<<<<<< HEAD
-      "The best for preparation tomorrow is doing you best today.";
+    quote.innerHTML = "The best for preparation tomorrow is doing you best today.";
     userImg.innerHTML = `<img class="card-img-top " src="img/sampleava.jpg" alt="Card image cap" />`;
     tweetImg.innerHTML = `<img
                   class="img-radius rounded float-left"
                   src="img/testava.jpg"
                   alt="ava"
               />`;
-=======
-      "The best for preparation tomorrow is doing your best today.";
->>>>>>> 8d141460abda21d6764ea88e325fd422defdbfce
+     
     isLogin = true;
     logOutbtn.innerHTML = "LOG OUT";
     closeForm("myForm");
